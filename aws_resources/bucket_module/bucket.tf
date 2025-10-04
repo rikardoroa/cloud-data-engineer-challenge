1e9eb2a2181d245d7933a9287cfaf0095b23dc53
Ricardo Roa <32416860+rikardoroa@users.noreply.github.com>
2025-10-04 14:21:33 -0500

    Update bucket.tf

--- aws_resources/bucket_module/bucket.tf
+++ aws_resources/bucket_module/bucket.tf
@@ -2,26 +2,25 @@ data "aws_caller_identity" "current" {}
 
 # Creating the KMS key resource
 resource "aws_kms_key" "dts_kms_key" {
-  description = "Key for encryption"
+  description         = "Key for encryption"
   enable_key_rotation = true
-  key_spec = "SYMMETRIC_DEFAULT"
+  key_spec            = "SYMMETRIC_DEFAULT"
 }
 
 # Activating KMS key policy
 resource "aws_kms_key_policy" "bucket_kms_key" {
   key_id = aws_kms_key.dts_kms_key.id
-
   policy = jsonencode({
     Version = "2012-10-17"
-    Id = "key-default-1"
+    Id      = "key-default-1"
     Statement = [
       {
-        Sid = "Enable IAM User Permissions"
+        Sid    = "Enable IAM User Permissions"
         Effect = "Allow"
         Principal = {
           AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
         }
-        Action = "kms:*"
+        Action   = "kms:*"
         Resource = "*"
       }
     ]
@@ -30,22 +29,23 @@ resource "aws_kms_key_policy" "bucket_kms_key" {
 
 # Creating KMS key alias
 resource "aws_kms_alias" "dts_kms_alias" {
-  name = "alias/mv-pr-dt-key"
+  name          = "alias/mv-pr-dt-key"
   target_key_id = aws_kms_key.dts_kms_key.key_id
 }
 
 # Creating the S3 bucket
 resource "aws_s3_bucket" "bucket_creation" {
-  bucket = var.curated_bucket
+  bucket        = var.curated_bucket
   force_destroy = true
 }
 
 # Setting bucket access
 resource "aws_s3_bucket_public_access_block" "public_access_block" {
   bucket = aws_s3_bucket.bucket_creation.id
-  block_public_acls = true
-  block_public_policy = true
-  ignore_public_acls = true
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
   restrict_public_buckets = true
 }
 
@@ -55,7 +55,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "ss_kms_key" {
 
   rule {
     apply_server_side_encryption_by_default {
-      sse_algorithm = "aws:kms"
+      sse_algorithm     = "aws:kms"
       kms_master_key_id = aws_kms_key.dts_kms_key.arn
     }
   }
