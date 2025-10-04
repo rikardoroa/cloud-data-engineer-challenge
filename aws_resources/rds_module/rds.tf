cea929343a503e042ffed88c148a2c2014ff6db0
Ricardo Roa <32416860+rikardoroa@users.noreply.github.com>
2025-10-04 14:33:20 -0500

    Update rds.tf

--- aws_resources/rds_module/rds.tf
+++ aws_resources/rds_module/rds.tf
@@ -1,5 +1,5 @@
 resource "aws_db_instance" "postgres" {
-  engine                 = "Postgres"
+  engine                 = "postgres"
   identifier             = "dbgeospatialdev"
   allocated_storage      = 20
   engine_version         = "15.7"
