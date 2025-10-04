3a4fda6fea60988c5ef2d80af2692bf8fdcb443b
Ricardo Roa <32416860+rikardoroa@users.noreply.github.com>
2025-10-03 20:17:06 -0500

    updating workflows

--- aws_resources/rds_module/rds.tf
+++ aws_resources/rds_module/rds.tf
@@ -1,7 +1,7 @@
 resource "aws_db_instance" "postgres" { 
     identifier = "mi-postgis-db" 
     engine = "postgres" 
-    engine_version = "15.5" 
+    engine_version = "15.7" 
     instance_class = "db.t3.micro" 
     allocated_storage = 20 
     db_name = var.db_name
