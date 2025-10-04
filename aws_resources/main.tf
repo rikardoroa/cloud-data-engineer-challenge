49f12981a27523e2791c61e84ff61471e8e6a1a8
Ricardo Roa <32416860+rikardoroa@users.noreply.github.com>
2025-10-04 14:31:38 -0500

    Update main.tf

--- aws_resources/main.tf
+++ aws_resources/main.tf
@@ -10,13 +10,13 @@ module "aws_network_utils" {
 
 # lambda module
 module "aws_lambda_utils" {
-  source                 = "./lambda_module"
-  target_bucket          = module.bucket_utils.target_bucket
-  target_key             = module.bucket_utils.target_key
-  bucket_arn             = module.bucket_utils.bucket_arn
-  bucket_id              = module.bucket_utils.bucket_id
-  security_group_lambda  = module.aws_network_utils.security_group_lambda
-  subnet2                = module.aws_network_utils.subnet2
+  source                = "./lambda_module"
+  target_bucket         = module.bucket_utils.target_bucket
+  target_key            = module.bucket_utils.target_key
+  bucket_arn            = module.bucket_utils.bucket_arn
+  bucket_id             = module.bucket_utils.bucket_id
+  security_group_lambda = module.aws_network_utils.security_group_lambda
+  subnet2               = module.aws_network_utils.subnet2
 }
 
 # RDS module
