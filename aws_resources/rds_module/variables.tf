96da247b7bd109129c5ee78e217bb3d2a5f8e5e6
Ricardo Roa <32416860+rikardoroa@users.noreply.github.com>
2025-10-04 15:49:16 -0500

    updating repo

--- aws_resources/rds_module/variables.tf
+++ aws_resources/rds_module/variables.tf
@@ -1,14 +1,6 @@
-variable "db_user" {
-  type = string
-}
-
 variable "db_password" {
   type      = string
   sensitive = true
-}
-
-variable "db_name" {
-  type = string
 }
 
 variable "security_group" {
