ca638480061161ae5fe2c70737365fbb6032b213
Ricardo Roa <32416860+rikardoroa@users.noreply.github.com>
2025-10-04 13:53:01 -0500

    updating iam.tf

--- aws_resources/lambda_module/iam.tf
+++ aws_resources/lambda_module/iam.tf
@@ -19,8 +19,8 @@ resource "aws_iam_role" "iam_dev_role_pr_mv" {
 # Lambda Policy Document
 data "aws_iam_policy_document" "pipeline_dev_policy_pr_mv" {
   statement {
-    sid    = "CloudWatchLogging"
-    effect = "Allow"
+    sid     = "CloudWatchLogging"
+    effect  = "Allow"
     actions = [
       "logs:DescribeLogGroups",
       "logs:DescribeLogStreams",
@@ -31,14 +31,14 @@ data "aws_iam_policy_document" "pipeline_dev_policy_pr_mv" {
       "logs:PutLogEvents",
       "logs:PutRetentionPolicy",
       "logs:DeleteLogGroup",
-      "logs:DeleteLogStream"
+      "logs:DeleteLogStream",
     ]
     resources = ["*"]
   }
 
   statement {
-    sid    = "S3AndKMSAccess"
-    effect = "Allow"
+    sid     = "S3AndKMSAccess"
+    effect  = "Allow"
     actions = [
       "s3:ListBucket",
       "s3:GetBucketLocation",
@@ -47,29 +47,29 @@ data "aws_iam_policy_document" "pipeline_dev_policy_pr_mv" {
       "s3:PutObject",
       "s3:GetObject",
       "s3:DeleteObject",
-      "kms:*"
+      "kms:*",
     ]
     resources = ["*"]
   }
 
   statement {
-    sid    = "SecretsManagerAccess"
-    effect = "Allow"
+    sid     = "SecretsManagerAccess"
+    effect  = "Allow"
     actions = [
-      "secretsmanager:GetSecretValue"
+      "secretsmanager:GetSecretValue",
     ]
     resources = ["*"]
   }
 
   statement {
-    sid    = "EC2NetworkInterfaceAccess"
-    effect = "Allow"
+    sid     = "EC2NetworkInterfaceAccess"
+    effect  = "Allow"
     actions = [
       "ec2:CreateNetworkInterface",
       "ec2:DescribeNetworkInterfaces",
       "ec2:DeleteNetworkInterface",
       "ec2:AssignPrivateIpAddresses",
-      "ec2:UnassignPrivateIpAddresses"
+      "ec2:UnassignPrivateIpAddresses",
     ]
     resources = ["*"]
   }
