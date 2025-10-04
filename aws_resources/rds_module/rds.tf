c7f284aaa241af0f9db45d597ede3527589914bd
Ricardo Roa <32416860+rikardoroa@users.noreply.github.com>
2025-10-04 14:43:02 -0500

    Update rds.tf

--- aws_resources/rds_module/rds.tf
+++ aws_resources/rds_module/rds.tf
@@ -1,18 +1,18 @@
 resource "aws_db_instance" "postgres" {
-  engine = "postgres"
-  identifier = "dbgeospatialdev"
-  allocated_storage = 20
-  engine_version = "15.7"
-  instance_class = "db.t3.micro"
-  username = "postgres"
-  password = var.db_password
+  engine                 = "postgres"
+  identifier             = "dbgeospatialdev"
+  allocated_storage      = 20
+  engine_version         = "15.7"
+  instance_class         = "db.t3.micro"
+  username               = "postgres"
+  password               = var.db_password
   vpc_security_group_ids = [var.security_group]
-  db_subnet_group_name = var.subnet_group
-  skip_final_snapshot = true
+  db_subnet_group_name   = var.subnet_group
+  skip_final_snapshot    = true
 }
 
 resource "aws_secretsmanager_secret" "rds_secret" {
-  name = "postgresql_conn"
+  name        = "postgresql_conn"
   description = "RDS credentials for geospatialdev database"
 }
 
@@ -21,8 +21,8 @@ resource "aws_secretsmanager_secret_version" "rds_secret_value" {
   secret_string = jsonencode({
     username = "postgres"
     password = var.db_password
-    host = aws_db_instance.postgres.endpoint
-    port = aws_db_instance.postgres.port
-    dbname = "postgres"
+    host     = aws_db_instance.postgres.endpoint
+    port     = aws_db_instance.postgres.port
+    dbname   = "postgres"
   })
 }
