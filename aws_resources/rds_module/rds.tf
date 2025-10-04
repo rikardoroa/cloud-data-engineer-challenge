06e57cda437e1d07261fd966a0b512fb27b0c472
Ricardo Roa <32416860+rikardoroa@users.noreply.github.com>
2025-10-03 20:59:03 -0500

    Update rds.tf

--- aws_resources/rds_module/rds.tf
+++ aws_resources/rds_module/rds.tf
@@ -1,11 +1,10 @@
 resource "aws_db_instance" "postgres" { 
+    engine = "Postgres" 
     identifier = "dbgeospatialdev"
-    engine = "postgres" 
-    engine_version = "15.7" 
-    instance_class = "db.t3.micro" 
     allocated_storage = 20 
-    db_name = var.db_name
-    username = var.db_user
+    engine_version = "15.7" 
+    instance_class = "db.t3.micro"
+    username = var.db_user 
     password = var.db_password
     vpc_security_group_ids = [var.security_group] 
     db_subnet_group_name = var.subnet_group
