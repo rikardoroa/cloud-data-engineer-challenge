8e625e189dac7cfed95f669d3cdbedcfb04dad4d
Ricardo Roa <32416860+rikardoroa@users.noreply.github.com>
2025-10-05 12:12:37 -0500

    Update rds.tf

--- aws_resources/rds_module/rds.tf
+++ aws_resources/rds_module/rds.tf
@@ -11,7 +11,7 @@ resource "aws_db_instance" "postgres" {
   skip_final_snapshot        = true
   storage_encrypted          = true
   backup_retention_period    = 7            
-  backup_window    = "03:00-04:00" 
+  backup_window              = "03:00-04:00" 
   deletion_protection        = true         
   auto_minor_version_upgrade = true      
 }
