aec559234491307671b3c11f2a61e88c97a250d7
Ricardo Roa <32416860+rikardoroa@users.noreply.github.com>
2025-10-05 13:18:52 -0500

    updating repo

--- aws_resources/main.tf
+++ aws_resources/main.tf
@@ -1,17 +1,11 @@
 # bucket module
 module "bucket_utils" {
   source = "./bucket_module"
-  providers = {
-    aws = aws
-  }
 }
 
 # network module
 module "aws_network_utils" {
   source = "./network_module"
-  providers = {
-    aws = aws
-  }
 }
 
 # lambda module
@@ -35,9 +29,6 @@ module "aws_rds_utils" {
   db_password    = var.db_password
   security_group = module.aws_network_utils.security_group
   subnet_group   = module.aws_network_utils.subnet_group
-  providers = {
-    aws = aws
-  }
 }
 
 # API Gateway module
@@ -45,7 +36,4 @@ module "aws_api_gateway_utils" {
   source        = "./api_gateway_module"
   invoke_arn    = module.aws_lambda_utils.invoke_arn
   function_name = module.aws_lambda_utils.lambda_function
-  providers = {
-    aws = aws
-  }
 }
