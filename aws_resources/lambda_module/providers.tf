6d3d6552ecdc4f41944f78c9496f6c63f0c4b5ce
Ricardo Roa <32416860+rikardoroa@users.noreply.github.com>
2025-10-05 13:35:58 -0500

    updating repo

--- aws_resources/lambda_module/providers.tf
+++ aws_resources/lambda_module/providers.tf
@@ -5,6 +5,14 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.60.0"
     }
+    archive = {
+      source  = "hashicorp/archive"
+      version = "~> 2.7.1"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 3.2.4"
+    }
     docker = {
       source  = "kreuzwerker/docker"
       version = "~> 3.6.2"
